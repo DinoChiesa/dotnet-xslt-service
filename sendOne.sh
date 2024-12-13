70bf6281746d80a48ea998310256f6712c2acf9a
DinoChiesa <dchiesa@google.com>
2024-12-13 06:35:24 +0000

    feat: cache the remote URL in sendOne.sh

--- sendOne.sh
+++ sendOne.sh
@@ -18,17 +18,65 @@ getRandomContentFile() {
   echo "${random_file}"
 }
 
+getRemoteEndpoint_NoCache() {
+  gcloud run services describe "${SERVICE}" \
+    --region "${REGION}" --project="${PROJECT}" \
+    --format 'value(status.url)'
+}
+
+getRemoteEndpoint() {
+  local current_timestamp fresh_filename files ENDPOINT ftimestamp time_diff
+
+  current_timestamp=$(date +%s)
+  fresh_filename=".remote-endpoint-${current_timestamp}"
+
+  # Enable nullglob to handle cases with no matching files
+  shopt -s nullglob
+
+  # get all files matching a filespec, or empty set if none
+  files=(.remote-endpoint-*)
+
+  if [[ ${#files[@]} -eq 0 ]]; then
+    ENDPOINT=$(getRemoteEndpoint_NoCache)
+
+  elif [[ ${#files[@]} -eq 1 && -f "${files[0]}" ]]; then
+    # There is only one file. Is it "fresh"?
+    # Extract the timestamp from the filename.
+    ftimestamp=$(echo "${files[0]}" | grep -oE '[0-9]{10}')
+    time_diff=$((current_timestamp - ftimestamp))
+
+    if [[ $time_diff -gt 115 ]]; then
+      rm -f "${files[0]}"
+      ENDPOINT=$(getRemoteEndpoint_NoCache)
+    else
+      # The file is recent. Rename it to keep it fresh, and return it.
+      mv "${files[0]}" $fresh_filename
+      ENDPOINT=$(cat "$fresh_filename")
+    fi
+
+  else
+    # There is more than one file. Remove all and start again.
+    for file in "${files[@]}"; do
+      rm -f "$file"
+    done
+    ENDPOINT=$(getRemoteEndpoint_NoCache)
+  fi
+
+  [[ ! -f "$fresh_filename" ]] && echo "$ENDPOINT" >$fresh_filename
+  echo $ENDPOINT
+}
+
+if [[ "${TARGET}" = "local" ]]; then
+  ENDPOINT="0:9090"
+elif [[ -z "${TARGET}" ]]; then
+  ENDPOINT=$(getRemoteEndpoint)
+fi
+
 randomfile=$(getRandomContentFile)
 printf "Selected %s\n" "${randomfile}"
 
 printf "\ninput:\n"
 cat "${randomfile}"
-
-if [[ "${TARGET}" = "local" ]]; then
-  ENDPOINT="0:9090"
-elif [[ -z "${TARGET}" ]]; then
-  ENDPOINT=$(gcloud run services describe "${SERVICE}" --region "${REGION}" --project="${PROJECT}" --format 'value(status.url)')
-fi
 
 printf "\n\nsending to target: %s\n" "$ENDPOINT"
 
